d853eae3fb34e0b033782fb4c3fec856f112a074
Florian Rusch <git@florianrusch.de>
2021-06-22 09:26:17 +0200

    Formatting

---
+++ src/components/Form/TextInput.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex items-center border-b border-teal-500 py-2">
+		<input
+			class="bg-transparent border-none mr-3 px-2 leading-tight focus:outline-none"
+			type="text"
+			placeholder=""
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+		return {}
+	},
+})
+</script>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,34 +1,34 @@
 <template>
-  <tr>
-    <td class="text-left py-3 px-4">{{ task.task }}</td>
-    <td class="text-left py-3 px-4">{{ task.complexity }}</td>
-    <td class="text-left py-3 px-4">{{ task.uncertainty }}</td>
-    <td class="text-left py-3 px-4">{{ min }} days</td>
-    <td class="text-left py-3 px-4">{{ max }} days</td>
-    <td class="text-left py-3 px-4">{{ task.indeed }} days</td>
-  </tr>
+	<tr>
+		<td class="text-left py-3 px-4">{{ task.task }}</td>
+		<td class="text-left py-3 px-4">{{ task.complexity }}</td>
+		<td class="text-left py-3 px-4">{{ task.uncertainty }}</td>
+		<td class="text-left py-3 px-4">{{ min }} days</td>
+		<td class="text-left py-3 px-4">{{ max }} days</td>
+		<td class="text-left py-3 px-4">{{ task.indeed }} days</td>
+	</tr>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from "vue";
-import { Task as TaskModel } from "@/models/Task";
+import { defineComponent, toRefs } from 'vue'
+import { Task as TaskModel } from '@/models/Task'
 
 export default defineComponent({
-  props: {
-    task: {
-      type: TaskModel,
-      required: true,
-    },
-  },
+	props: {
+		task: {
+			type: TaskModel,
+			required: true,
+		},
+	},
 
-  setup(props) {
-    const { task } = toRefs(props);
-    const min = task.value.complexity;
-    const max = task.value.complexity * task.value.uncertainty;
-    return {
-      min,
-      max,
-    };
-  },
-});
+	setup(props) {
+		const { task } = toRefs(props)
+		const min = task.value.complexity
+		const max = task.value.complexity * task.value.uncertainty
+		return {
+			min,
+			max,
+		}
+	},
+})
 </script>
